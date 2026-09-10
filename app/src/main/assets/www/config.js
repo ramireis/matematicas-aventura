838251c7d222281336b6196f65a88d0dd359c11c
ramireis <ramireis777@hotmail.com>
2026-09-10 06:58:26 -0500

    Android 4.1.0: agregar app/src/main/assets/www/config.js

---
+++ app/src/main/assets/www/config.js
@@ -0,0 +1,4 @@
+/* Configure únicamente la URL pública HTTPS de Cloud Run. No coloque claves aquí. */
+window.MZ_CONFIG=Object.freeze({
+  API_BASE_URL:'https://REEMPLAZAR-POR-SERVIDOR-CLOUD-RUN'
+});
