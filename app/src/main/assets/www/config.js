ee4cbf415f32f72d89b661a09889750e630303cc
ramireis <ramireis777@hotmail.com>
2026-09-10 07:19:38 -0500

    Android 4.1.0 estudiante: retirar recurso heredado app/src/main/assets/www/config.js

--- app/src/main/assets/www/config.js
+++
@@ -1,4 +0,0 @@
-/* Configure únicamente la URL pública HTTPS de Cloud Run. No coloque claves aquí. */
-window.MZ_CONFIG=Object.freeze({
-  API_BASE_URL:'https://REEMPLAZAR-POR-SERVIDOR-CLOUD-RUN'
-});
