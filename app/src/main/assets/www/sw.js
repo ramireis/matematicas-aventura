6e4c8718422665b23e4bded1c228acb02f3dfae4
ramireis <ramireis777@hotmail.com>
2026-09-10 07:19:54 -0500

    Android 4.1.0 estudiante: retirar recurso heredado app/src/main/assets/www/sw.js

--- app/src/main/assets/www/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='mate-zombis-v3.0.1-secure';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./config.js','./manifest.webmanifest','./qrcode.js']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
